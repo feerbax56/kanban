5a34eea7d0615003d7644a6b0ebccf522b1d4c68
ÐAnton Evseev <soki1997@mail.ru>
2023-03-16 14:37:27 +0500

    add: array of tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainViewComponent} from './pages/main-view/main-view.component';
-import {BoardComponent} from './models/board/board.component';
 import {CdkDropList, CdkDropListGroup, DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainViewComponent,
-    BoardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {Board} from "../../models/board.model";
+import {Column} from "../../models/column.model";
 
 
 
@@ -9,9 +11,21 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./main-view.component.scss'],
 })
 export class MainViewComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  board: Board = new Board('testBoard', [
+    new Column( 'GetStarted', [
+      'one', 'duos', 'tree'
+    ] ),
+    new Column( 'Plus one', [
+      'firefox', 'Chromium', 'Opera'
+    ] ),
+    new Column('todo', [
+      'Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'
+    ]),
+    new Column('done',[
+      'Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'
+    ])
+  ])
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
